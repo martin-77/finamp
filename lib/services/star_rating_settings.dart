80903ab003d3746593e2eb6adcd4241d8c24f7a8
martin-77 <52350479+martin-77@users.noreply.github.com>
2026-08-21 20:54:40 +0200

    Add optional half-star ratings setting

--- lib/services/star_rating_settings.dart
+++ lib/services/star_rating_settings.dart
@@ -1,15 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 const _showStarRatingsPreferenceKey = 'showStarRatings';
+const _allowHalfStarRatingsPreferenceKey = 'allowHalfStarRatings';
 
 final showStarRatingsProvider = FutureProvider<bool>((ref) async {
   final preferences = await SharedPreferences.getInstance();
   return preferences.getBool(_showStarRatingsPreferenceKey) ?? false;
 });
 
+final allowHalfStarRatingsProvider = FutureProvider<bool>((ref) async {
+  final preferences = await SharedPreferences.getInstance();
+  return preferences.getBool(_allowHalfStarRatingsPreferenceKey) ?? false;
+});
+
+Future<bool> getAllowHalfStarRatings() async {
+  final preferences = await SharedPreferences.getInstance();
+  return preferences.getBool(_allowHalfStarRatingsPreferenceKey) ?? false;
+}
+
 Future<void> setShowStarRatings(WidgetRef ref, bool enabled) async {
   final preferences = await SharedPreferences.getInstance();
   await preferences.setBool(_showStarRatingsPreferenceKey, enabled);
   ref.invalidate(showStarRatingsProvider);
+
+  if (Platform.isIOS) {
+    const channel = MethodChannel('com.unicornsonlsd.finamp-ios/rating');
+    await channel.invokeMethod('setEnabled', {'enabled': enabled});
+  }
+}
+
+Future<void> setAllowHalfStarRatings(WidgetRef ref, bool enabled) async {
+  final preferences = await SharedPreferences.getInstance();
+  await preferences.setBool(_allowHalfStarRatingsPreferenceKey, enabled);
+  ref.invalidate(allowHalfStarRatingsProvider);
 }
